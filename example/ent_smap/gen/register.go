fe8bdb6ca786fae3289f19bda82ac80dc89d00c4
安博微 <boweian@creditease.cn>
2021-03-08 18:53:43 +0800

    add

--- example/ent_smap/gen/register.go
+++ example/ent_smap/gen/register.go
@@ -1,8 +1,37 @@
 package gen
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterAll(c *gin.Engine) {
-	c.GET("/post", Hello)
-	c.POST("/post",Hello)
+
+	c.GET("/car", func(c *gin.Context) {
+		GinResult(c, Car)
+	})
+
+	c.GET("/user", func(c *gin.Context) {
+		GinResult(c, Hello)
+	})
+
+}
+
+type Result struct {
+	Code int `json:"code"`
+	Data interface{}
+	Err  error
+}
+
+func GinResult(c *gin.Context, fc func(c *gin.Context) (data interface{}, err error)) {
+	data, err := fc(c)
+	res := Result{
+		Code: 0,
+		Data: data,
+		Err:  err,
+	}
+	if err != nil {
+		res.Code = 503
+	} else {
+		res.Code = 200
+	}
 }
