5f500025764d2e1fde8dde3d5afeb98121ba8527
安博微 <boweian@creditease.cn>
2021-07-26 18:27:10 +0800

    add

--- example/cmdb/logic/casbin/authorization.go
+++ example/cmdb/logic/casbin/authorization.go
@@ -124,7 +124,7 @@ func DeletePolicies(c *gin.Context, in *PoliciesIn) (bool, error) {
 type GetServiceTreeIn struct {
 
 }
-
+// @GenApi /api/tree [get]
 func GetServiceTree(c *gin.Context, in *GetServiceTreeIn) ([]*ent.ServiceTree, error) {
 	return public.GetDB().ServiceTree.Query().Where(servicetree.Not(servicetree.HasProject())).WithService().All(context.Background())
 }
@@ -133,8 +133,9 @@ type CreateProjectIn struct {
 	Body ent.ServiceTree `json:"body"`
 }
 
-func CreateProject(c *gin.Context, in CreateProjectIn) (*ent.ServiceTree, error) {
-	return public.GetDB().ServiceTree.Create().SetProject(&in.Body).Save(context.Background())
+// @GenApi /api/project [post]
+func CreateProject(c *gin.Context, in *CreateProjectIn) (*ent.ServiceTree, error) {
+	return public.GetDB().ServiceTree.Create().SetName(in.Body.Name).SetNote(in.Body.Note).SetType(in.Body.Type).Save(context.Background())
 }
 
 type CreateServiceIn struct {
@@ -144,7 +145,8 @@ type CreateServiceIn struct {
 	Body ent.ServiceTree `json:"body"`
 }
 
+// @GenApi /api/project/{id}/service [post]
 func CreateService(c *gin.Context, in *CreateServiceIn) (*ent.ServiceTree, error) {
-	return public.GetDB().ServiceTree.Create().SetProjectID(in.Uri.Id).AddService(&in.Body).Save(context.Background())
+	return public.GetDB().ServiceTree.Create().SetProjectID(in.Uri.Id).SetName(in.Body.Name).SetNote(in.Body.Note).SetType(in.Body.Type).Save(context.Background())
 }
 
