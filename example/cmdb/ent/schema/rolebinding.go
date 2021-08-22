03b9499ef7c27bda768dad6c4443aad11f6d0ab0
安博微 <boweian@creditease.cn>
2021-08-23 01:11:53 +0800

    add

--- example/cmdb/ent/schema/rolebinding.go
+++ example/cmdb/ent/schema/rolebinding.go
@@ -3,6 +3,7 @@ package schema
 import (
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"github.com/fitan/genapi/pkg/gen_mgr"
 	"time"
 )
 
@@ -14,9 +15,18 @@ type RoleBinding struct {
 // Fields of the RoleBinding.
 func (RoleBinding) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("role_name").Unique(),
-		field.String("role_id").Unique(),
-		field.Bool("status"),
+		field.String("role_name").Unique().Annotations(gen_mgr.RestFieldOp{
+			FieldQueryable:   gen_mgr.FieldQueryable{EQ: gen_mgr.GenRestTrue, ContainsFold: gen_mgr.GenRestTrue},
+			FieldOperability: gen_mgr.FieldOperability{},
+		}),
+		field.String("role_id").Unique().Annotations(gen_mgr.RestFieldOp{
+			FieldQueryable:   gen_mgr.FieldQueryable{EQ: gen_mgr.GenRestTrue},
+			FieldOperability: gen_mgr.FieldOperability{},
+		}),
+		field.Bool("status").Annotations(gen_mgr.RestFieldOp{
+			FieldQueryable:   gen_mgr.FieldQueryable{EQ: gen_mgr.GenRestTrue},
+			FieldOperability: gen_mgr.FieldOperability{},
+		}),
 		field.Time("created_at").Default(time.Now).Immutable().Optional().Nillable(),
 		field.Text("note"),
 		field.Strings("permissions"),
